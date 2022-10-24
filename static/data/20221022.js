26ddbc722dc9d6abc12603a7c63049dd882c0ab3
kaiking <1483612524@qq.com>
2022-10-24 15:13:34 +0800

    'update'

--- static/data/20221022.js
+++ static/data/20221022.js
@@ -1,6 +1,6 @@
 
 /** 正确答  */
-var rigthAnswer = ' DCADD  A (BCE) CCB'
+var rigthAnswer = ' CAACC  BDCBC'
 
 var rightAnswerA = `
 d	9.4
@@ -15,16 +15,44 @@ c	1.2.1
 b	11.7.1.2
 `
 
-
 /** 接龙数据 */
-
-
 var allData = `
 
-1. 卢维荣 CAADD A（BCE)ACB
-
-
-
+1. 林英 DBACA  CAAAB
+2. 易江涛 CCACC BDCBC
+3. 王鑫 BCACD BCCBB
+4. 陈少芬 CAACC BDCBC
+5. 黄雅君 BCACC BCCBC
+6. 郑鸿斌 CBACCBDABC
+7. 熊佳 CDACA CDABC
+8. 卢维荣 CDACC BDCAC
+9. 等等 CDCCC BCBBC
+10. 李翠艳CCABB BDBAB
+11. 旭BDCCAADAAC
+12. 陈小棋 CDACC BDABC
+13. 刘思婷 CDACCBDABC
+14. 钟洁莹 BCACD BDCBB
+15. 袁天赐 bcabc  bdcac
+16. 莫德坚 DBCBC BACAC
+17. 赵艳焜 ACACC BDCBC
+18. 阮晓梦 BCACA  BCCBC
+19. 唐泷彪 CCACB  BDCBC
+20. 陈庆娇 BBABD CDABC
+21. 谢成寿 BAACC BDAAC
+22. 雪落无痕 CCCCA BCABB
+23. 李凯旋 DCCCB DCABC
+24. 洪俊鑫 CADBA CACBC
+25. 陈书婷 BBCAA BDCBC
+26. 于涛 DCCCA CDCBC
+27. 汪关心 BACCC. BDCBB
+28. 艾克丝BAACD BDCBB
+29. 陈妙玲 DCACC BDCBC
+30. 温炳辉 CAACC BDCBC
+31. 陈永发 CBCCD BBCBC
+32. NICOLE CAACC BDCBC
+33. 慌、如隔世 CACADDBCAB
+34. 李勇发 CBABD CBABA
+35. @%@ BCDCA DBCBB
 `
 
 /**
@@ -33,168 +61,185 @@ var allData = `
  * 答案间隔符： |*|*|
  * **/
 var testQuestion = `
-1、 一位敏捷教练被分配到一个新成立的团队中，负责他们的第一个项目。教练安排了团队，提供了关于敏捷最佳实践的培训，并解释说敏捷团队是自我管理的。然而，团队成员正在努力从看板面板分配用户故事，并且由于缺乏协作，生产力正在受到影响。敏捷教练的最佳行动方案是什么？
-An agile coach has been assigned to a newly formed team with their first project. The coach colocated the team, provided training on agile best practices, and explained that agile teams are self-managing. However, the team members are struggling to assign user stories from the Kanban board, and productivity is suffering as a result of the lack of collaboration. What is the best course of action for the agile coach?
 
- A ：忽略这个问题，因为敏捷团队应该是自我管理的 Ignore the problem as agile teams are supposed to be self-managing.
+1、  在一个公路施工项中，最近出现了返工的情况，导致项目经理非常担心项目的不一致性成本。项目经理来寻求你的帮助。你能给他的最好的建议是？
+In a road construction project, the rework that happened lately makes the project manager very concerned about the inconsistency cost of no-conformance. The project manager turns to you for help. What is the best advice you can offer?
 
- B ：从现在开始，将用户故事标记给各个团队成员 Assign the user stories to individual team members from now onward
+ A ：重新进行规划质量管理过程 To re-plan the quality management process
 
- C ：将团队章程替换为更有生产力的项目团队的章程 Replace the team charter with one from a more productive project team.
+ B ：加强质量控制 To strengthen the quality control
 
- D ： 采取更直接的方法，由于团队仍处于形成阶段 Take a more directive approach since the team is still in its forming stage.
+ C ：开展质量审计 To perform quality audit
 
-|*|*| 正确答案：D 
-解析：知识点出处： PMBOK 6th 页码：P338 章节：9.4 塔克曼阶梯理论：形成阶段 、 震荡阶段 、规范阶段、成熟阶段 、 解散阶段 形成阶段，敏捷教练应该指导团队。
+ D ：开展标杆对照 To carry out benchmarking control
 
-==-=--=
+|*|*|正确答案：C 
+解析：知识点出处： PMBOK 6th 页码：P295 章节：8.2.2.5 审计：采取后续措施纠正问题，可以降低质量成本，并提高发起人或客户对项目产品的接受度。 审计可以降低质量成本。
 
-2、 在新目标国家推出产品前不久，公司意识到该产品并不完全符合当地的数据隐私法。由于预算限制，公司管理层要求项目团队将可能的罚款成本与返工成本进行比较。返工的一部分包括向产品待办事项列表中添加新需求。在这种情况下，下面哪个选项最有可能帮助到你？
-Shortly before rolling out a product in a newly targeted country, a company realizes that the product does not fully comply with local data privacy laws. Due to budget constraints, the company management asks the project team to compare costs for possible fines with costs for rework. Part of the rework would involve adding new requirements to the product backlog. In this situation, which of the following is most likely to help?
-
- A ：重构 Refactoring
-
- B ：用户画像 Personas
-
- C ： 阶段关口 A phase gate
-
- D ：快速跟进 Fast tracking
-
-|*|*| 正确答案：C
-解析：知识点出处： PMBOK 6th 页码：P18 章节：1.2.4 阶段关口： 为做出进入下个阶段、进行整改或结束项目集或项目的决定，做的阶段末审查。 审查一个阶段末的问题，做成本效益分析，以决定下个 阶段的项目。
-
-==-=--=
-
-3、 随着项目的进展，偏差分析表明与绩效测量基准相比，实际的项目绩效会恶化。因此，对成本、进度和范围的估算不再有效。团队确定大量的新功能、变更请求和缺陷修复是差异背后的主要原因。项目经理要确保在实际进展的基础上对项目的剩余工作进行进一步的估算，最好的行动方案是什么？
-As the project progresses, the variance analysis shows that actual project performance compared to the performance measurement baseline deteriorates. As a result, estimates for cost, schedule, and scope are no longer valid. The team determines that a high volume of new features, change requests, and defect repairs are the main reason behind the variance. What is the project manager's best course of action to ensure that further estimates for the remainder of the work on the project are made based on real progress?
-
- A ： 将产品开发方法转换为通过短迭代来适应和度量进度 Switch the product development approach to adaptive and measure progress via short iterations
-
- B ：限制项目中允许的新功能、变更请求和缺陷修复的数量 Limit the number of new features, change requests, and defect repairs allowed on the project
-
- C ：请使用自下而上估算成本和进度计划估算技术以及三点估算范围 Use a bottom-up estimating technique for cost and schedule, and three-point estimating for scope
-
- D ：基于单个的基准而不是单一的综合基准来衡量进度 Measure progress based on the individual baselines instead of the single integrated baseline
-
-|*|*| 正确答案：A 
-解析：知识点出处： 敏捷实践指南 页码：P18 章节：3.1 四种生命周期特征。 变更过多，说明需求是动态的，不建议使用瀑布型。
-
-==-=--=
-
-4、 项目经理与多位高管相关方一起参与一个生命周期替换项目，其中一位相关方强烈反对该项目。若要获得该相关方的支持，项目经理应该怎么做？
-A project manager joins a life cycle replacement project with multiple executive stakeholders,one stakeholders is strongly against the project.What should the project manager do to gain the stakeholders'support ?
-
- A ：制定权力/影响力方格，以确定该相关方影响项目的能力并确认其支持 Develop A power/influence grid to determine the stakeholders’ ability to influence the project and confirm its support
-
- B ：创建相关方参与计划，以确定该相关方的项目支持水平 Create a stakeholder engagement plan to determine the stakeholder’s level of project support
-
- C ：将该问题升级上报给项目推动者，并请求替换一个支持该项目的相关方 Escalate the issue to a project champion and request an alternate stakeholder that will support the project
-
- D ： 执行相关方分析，以确定缺乏项目支持的原因并对这些原因进行优先级排序 Conduct a stakeholder analysis to identify and prioritize reasons for the lack of project support
-
-|*|*| 正确答案：D 
-解析：知识点出处： PMBOK 6th 页码：P512 章节：13.1.2.3 相关方分析： 相关方分析会产生相关方清单和关于相关方的各种信息， 例如，在组织内的位置、在项目中的角色、与项目的利害关系、期望、态度（对项目的支持程度），以及对项目信息的兴趣。 先进行相关方分 析，再更新参与 计划。B在D后面。 A确认其支持 不对。
-
-==-=--=
-
-5、  项目经理正在制定项目进度计划，并希望赋予团队比过去项目更多的决策权。他创建了一个甘特图，显示WBS工作包级别的活动，这些活动被分配给团队而不是个人。然后，他将工作包记录在一个看板面板的待办事项列表中。项目经理如何通过理想化的属性和行为使用鼓舞人心的激励来授权团队？
-A project manager is developing the project schedule and wants to empower the team with more decision making authority than past projects. He creates a Gantt chart that displays activities down to the work package level of the WBS, which are assigned to the team rather than individuals. He then records the work packages in the backlog of a Kanban board. How might the project manager use inspirational motivation to empower the team through idealized attributes and behaviors?
-
- A ：采取自由放任的领导风格 Adopt a Laissez-faire leadership style
-
- B ：融入仆人型领导者的要素 Incorporate the elements of a servant leader
-
- C ：利用例外管理 Utilize management by exception
-
- D ：使用变革型领导风格 Use a transformational leadership style
-
-|*|*| 正确答案：D 
-解析：知识点出处：敏捷实践指南 页码：P73 章节：6.1.2 变革就绪情况： • 积极明确的管理层支持； • 变革管理实践，包括沟通和引导； • 逐个项目应用敏捷实践 • 向团队增量地引入敏捷实践；以及 • 通过采取适用的敏捷技术和实践示范引导。 从题干看出，这明显是从瀑布型向敏捷型的变革，需要变革型领导风格。
 
 ==-=--=
 
 
-6、 项目经理前往另一个国家执行最终可交付成果的上线过程，到达后，项目经理得知，由于任务计划在当地假日期间完成，关键资源将不可用，而延迟上线将使最终项目的交付处于危险之中。若要避免这个问题，项目经理应该在项目开始时完成哪一项工作？
-The project manager travels to another country to perform the final deliverable on-line process. Upon arrival, the project manager learns that because the task is scheduled to be completed during the local holiday period, key resources will be unavailable, and the delayed on-line will make the final project delivery at risk.To avoid this problem, which task should the project manager complete at the beginning of the project?
+2、 一家公司已与一家水电站签订了软件开发合同。项目管理计划指定Scrum作为开发方法。在项目的几次冲刺阶段，监管机构通知执行机构，由于新的环境法，原本不包括在范围基准中的安全功能将需要被内置到软件中。项目经理的最佳行动方案是什么？
+A company has been contracted to develop software for a hydroelectric plant. The project management plan specifies Scrum as the development approach. Few sprints into the project, regulators notify the performing organization that due to new environmental laws, safety features originally not included in the scope baseline will need to be built into the software. What is the project manager's best course of action?
 
- A ：创建一份团队日历并围绕该信息创建项目进度计划 Create a team calendar and create a project schedule around that information
+ A ： 发出变更请求以更新项目管理计划，一旦获得批准，确保范围基准得到更新 Issue a change request to update the project management plan and, once approved, ensure that the scope baseline is updated
 
- B ：要求所有团队，无论在哪个国家，均遵循总部的日历 Require all teams, regardless of country, to follow the headquarters calendar
+ B ：与产品负责人进行根本原因分析，以确定为安全功能最初未包含在范围中的原因 Conduct a root cause analysts with the product owner to determine why the safety features were not included in the scope in the first place
 
- C ：制定加班预算以补偿在假期工作的资源 Develop an overtime budget to compensate for resources that work during the holidays
+ C ：使用MoSCoW优先级模型来梳理范围基准，并将新的安全功能分类为"应具有" Use the MoSCoW model of prioritization to groom the scope baseline and categorize the new safety features as "should have"
 
- D ：记录风险登记册中包含各个国家日历的影响 Record the effect of the national calendar in the risk register
+ D ：在冲刺计划期间与团队讨论新功能，如果团队同意，请他们在下次冲刺中实现功能 Discuss the new features with the team during sprint planning and if the team agrees, ask them to implement the features in the next sprint
 
-|*|*| 正确答案：A 
-解析：知识点出处： PMBOK 6th 页码：P323 章节：9.2.1.2 资源日历： 资源日历识别了每种具体资源 可用时的工作日、班次、正常营业的上下班时间、周末和公共假期。在规划活动期间，潜在的可用资源信息（如团队资源、设备和材料）用于估算资源可用性。资源日历还规定了在项目期间确定的团队和实物 资源何时可用、可用多久。 何时可用、 可用多久
+|*|*|正确答案：A 
+解析：知识点出处： PMBOK 6th 页码：P113 章节：4.6 变更流程：变更提出后，先分析影响，再提交含解决措施的变更请求给 CCB 存在范围基准，说明使用了混合方法 改变了范围基准，需要提交变更请求。
 
-==-=--=
-
-7、  [多选] 为了生产可交付的产品，你的项目必须与几个供应商签订合同。可交付成果将使用一个敏捷框架进行开发，该框架采用约束驱动的交付方式，这将影响与供应商的合同关系。 以下哪一种策略最适合这个项目?(选择三个)
-To produce deliverables, your project has to contract with several vendors. The deliverables will be developed using an agile framework with constraint-driven delivery, which will influence the contractual relationship with the vendors. Which of the following strategies would be the most appropriate for this project? (Choose three)?
-
- A ：根据详细的工作分解结构制定采购工作说明书 Develop the procurement statement of work based on the detailed WBS
-
- B ：追求与供应商共享风险和回报的关系。 Pursue a shared-risk-reward relationship with the vendors
-
- C ：与供应商签订合同时，采用固定价格增量。 Adopt fixed-price increments when contracting with the vendors
-
- D ：对每个供应商使用一个标准的固定价格合同。 Utilize a standard firm-fixed-price contract for each of the vendors
-
- E ：在供应商合同中包含提前取消的选项。 Include an early cancellation option in the vendor contracts
-
-|*|*| 正确答案：B,C,E 
-解析：知识点出处：敏捷实践指南 页码：P77-78 章节：6.3 采购和合同： • 多层结构 • 强调价值交付 • 总价增量 • 固定时间和材料 • 累进的时间和材料 • 提前取消方案 • 动态范围方案 • 团队扩充 • 支持全方位供应商 敏捷的采购和合同
 
 ==-=--=
 
-8、 你将与你的团队会面来确定项目的生命周期。在分析了定义和管理需求、开发可交付成果、处理变更、控制风险和成本，以及与关键相关方合作的最佳方式之后，做出了选择混合生命周期的决定。在选定项目生命周期后，关键相关方多久参与一次？
-You meet with your team to determine the life cycle for your project. After analyzing the best way to define and manage requirements, develop deliverables, handle changes, control risk and cost, and engage key stakeholders, the decision is made to select a hybrid life cycle. With the project life cycle selected, how often will the key stakeholders be involved?
 
- A ：持续参与 Continuously
+3、 由于没有空，具有批准权限的相关方拒绝了参与项目开工会议的邀请。该项目时间紧迫，及时开工对于满足截止日期至关重要。项目经理应该做什么？
+Due to the lack of time, interested parties with approval authority rejected the invitation to participate in the kick-off meeting. The project is urgent, and timely start-up is essential to meet the deadline. What should the project manager do?
 
- B ：在特定里程碑参与 At specific milestones
+ A ： 升级上报给项目发起人 The upgrade is reported to the project sponsor
 
- C ： 定期参与 Regularly
+ B ：在没有他们参加的情况下举行项目开工会议 Hold project kick-off meeting without their participation
 
- D ：完全不参与 Not involved at all
+ C ：将会议重新安排在他们能够参与的时间 Reschedule the meeting at a time when they can participate
 
-|*|*| 正确答案：C 
-解析：知识点出处： 敏捷实践指南 页码：P26 章 节：3.1.6 混合生命周期的特征： 对于整个项目，没有必要使用单一的方法。为达到特定的目标，项目经常要结合不同的生命周期要素。预测、 迭代、增量和/或敏捷方法的组合就是一种混合方法。 A是敏捷， B是预测。 而混合型建议定期参与。
+ D ：询问与会者以确定开工会议是否有必要 Ask participants to determine if a kick-off meeting is necessary
 
-==-=--=
+|*|*|正确答案：A 
+解析：知识点出处： PMBOK 6th 页码：P86 章节：4.2.2.4 Kick-off 会议：项目开工会议通常意味着规 划阶段结束和执行阶段开始， 旨在传达项目目标、获得团队对项目的承诺，以及阐明每个相关方的角色和职责。规划已经结束，就等着审批后执行了，此时应尽快解决。
 
-9、 客户希望增加股东权益的总和，提高其在全球市场的品牌知名度。客户的主要目标是什么？
-Customer wants to increase the total shareholders' equity and increase their brand awareness in the global market. What is the main objective of the customer?
-
- A ：为组织战略改善整体业务支持 To improve the overall business support for organizational strategy
-
- B ：增加营销预算 To increase the marketing budget
-
- C ：提高商业价值 To increase the business value
-
- D ：通过重新投资其他业务组合提高市场份额 To increase the market share by reinvesting in other business portfolios
-
-|*|*| 正确答案：C 
-解析：知识点出处： PMBOK 6th 页码：P7 章节：1.2.1 有形效益的例子包括： • 货币资产； • 股东权益； • 公共事业； • 固定设施； • 工具； • 市场份额。 股东权益属于有形效益。
 
 ==-=--=
 
-10、 在开发一个新产品时，耐久性测试是关键路径上的一项重要活动。然而，测试设施被一个优先级较高的项目占用，且占用时间比原计划长。项目经理接下来应该怎么做？
-In the development of a new project the endurance testing is an important activity on the critical path. However, the last facility is occupied by a higher project. Which is taking longer than initially planned. What should the project manager do text?
 
- A ：为延迟的项目开展根本原因分析 Perform a root cause analysis for the delayed project
+4、 项目经理正在制定一个项目计划。项目赞助人得知后，立即让项目经理针对项目成本开展初始粗略评估，以满足未来财政预算的需求。项目经理采用下列哪一项评估项目成本？
+The project manager is developing a project plan, on short notice project sponsor asks the project manager to create an initial rough estimate of project cost to satisfy requirements for the upcoming fiscal budget. The project manager uses which of the following for estimating the project cost?
 
- B ：审查风险登记册中的适当响应 Review the risk register for the appropriate response
+ A ：自下而上估算 Bottom-up estimating
 
- C ：将延期情况通知项目相关方 Inform the project stakeholders about the delay
+ B ：三点结算 Three-point estimating
 
- D ：将问题上报给高级管理层 Escalate the issue to senior management
+ C ： 类比估算 Analogous estimating
 
-|*|*| 正确答案：B 
-解析：知识点出处： PMBOK 6th 页码：P455 章节：11.7.1.2 风险登记册： • 已识别单个项目风险 • 风险责任人 • 商定的风险应对策略 • 以及具体的应对措施。 遇风险，先查册
- 
+ D ：建立参数模型 Parametric modeling
+
+|*|*|正确答案：C 
+解析：知识点出处： PMBOK 6th 页码：P244 章节：7.2.2.2 成本类比估算使用以往类似项目的参数值或属性来估算。项目的参数值和属性包括 （但不限于）范围、成本、预算、持续时间和规模指标， 类比估算以这些项目参数值 或属性为基础来估算当前项目的同类参数或指标。一般题目中出现粗略、需要快速估算、量级字眼，都是选 择类比估算
+
+
 ==-=--=
+
+
+
+5、  项目经理希望在新项目中使用一名特定供应商。该供应商目前正在为项目经理管理的另一个项目工作。项目经理希望在开始为新项目工作之前，先完成当前项目。在供应商开始为新项目工作之前，项目经理应该做什么？
+The project manager wants to use a specific vendor in the new project. The vendor is currently working on another project managed by the project manager. The project manager wants to complete the current project before starting work on the new project. What should the project manager do before the vendor starts working on the new project?
+
+ A ：与供应商一起评审合同协议。 Review contract agreements with suppliers
+
+ B ：更新采购文档。 Update purchase document
+
+ C ：执行采购审计。 Perform procurement audits
+
+ D ：要求供应商完成所有现有工作。 Require suppliers to complete all existing work
+
+|*|*|正确答案：C 
+解析：知识点出处： PMBOK 6th 页码：P498 章节：12.3.2.5 采购审计：审计是对采购过程的结构化审查。应该在采购合同中明确规定与审计有关的权利和义务。买方的项目经理和卖方的项目经理都应该关注审计结果，以便对项目进行必要调整。注意题干中提到的问题的逻辑关系
+
+
+==-=--=
+
+
+6、  项目经理加入一个项目，但项目经理在该项目所涉及的行业经验有限，在该项目的整个生命周期中，项目经理精心记录每个差距、问题和不一致性。但是，无论项目经理如何记录和跟踪生产问题，但问题都没有得到解决，这使项目交付面临风险。 若要解决这些问题，项目经理事先应该做什么？
+A project manager joins a project in an industry with which they have limited experience.Throughout the project's life cycle,the project manager meticulously documents each gap, problem,and inconsistency.However,production issues remain ,unresolved；regardless of the project manager's efforts to record and track them,which puts project delivery at risk. What should the project manager have done to resolve these issues?
+
+ A ：调整范围基准和项目进度计划与客户需求保持一致 Aligned the scope baseline and project schedule to the customer's needs
+
+ B ：使用主题专家(SMEs)提供适合的应对行动 Used subject matter experts(SMEs) to provide suitable response actions
+
+ C ：审查相关方的需求与验收标准相匹配 Reviewed stakeholder requirements to match the acceptance criteria
+
+ D ：检查需求跟踪矩阵，以确保它与可交付成果相联系 Examined the requirements tractability matrix to insure that it linked to the deliverables.
+
+|*|*|正确答案：B 
+解析：知识点出处： PMBOK 6th 页码：P94 章节：4.3.2.1 • 关于项目所在的行业以及项目关注的领域的技术知识； • 成本和预算管理； • 法规与采购； • 法律法规； • 组织治理。 看到行业领域的事情，一般都找专家判断。
+
+
+==-=--=
+
+
+7、  项目经理正在领导一个高度敏感的政府项目，按时交付该项目至关重要。第三方供应商按时交付一个关键项目组件，但不满足规范要求。目前尚不清楚交付的组件是否会按照批准的项目设计进行。项目经理应该做什么？
+The project manager is leading a highly sensitive government project, and delivering the project on time is critical. A third-party vendor delivered a critical project component on time, but did not meet the specification requirements. It is unclear whether the delivered components will follow the approved project design.What should the project manager do?
+
+ A ：接受该组件并按时交付该项目 Accept the component and deliver the project on time
+
+ B ：要求退还该项目的款项，并告知相关方该延迟情况 Request a refund of the project and inform the parties of the delay
+
+ C ：与客户协商接受不合格的组件 Negotiate with customers to accept substandard components
+
+ D ：拒绝该组件并重新订购，并且重新制定项目的进度计划 Reject the component and reorder, and re-schedule the project
+
+|*|*|正确答案：D 
+解析：知识点出处： PMBOK 6th 页码：P498 章节：12.3.2.3 绩效审查 ：对照协议，对质量、资源、进度和成本绩效进行测量、比较和分析， 以审查合同工作的绩效。其中包括确定工作包提前或落后于进度计划、超出或低于预算，以及是否存在 资源或质量问题。质量不合格，退了重新定。
+
+
+==-=--=
+
+
+8、  作为向敏捷转变的一部分，一个组织为一个项目选择一个试点团队来开发一个软件工具。一位敏捷教练被分配到团队中，在整个过渡过程中指导他们。在最初的几次迭代中，教练与项目经理和团队一起指导他们进行敏捷实践，并将任务分配给团队成员。敏捷教练采用什么最佳行动方案来确定团队是否获得了在没有教练的情况下执行即将到来的迭代所需的技能？
+As part of its transformation to agile, an organization selects a pilot team for a project to develop a software tool. An agile coach is assigned to the team to guide them throughout their transition. For the first few iterations, the coach works with the project manager and team to coach them on agile practices and assign tasks to the team members. What is agile coach's best course of action to determine if the team has acquired the required skills to perform the upcoming iterations without coaching?
+
+ A ：参加每日站会 Attend daily standup meetings
+
+ B ：进行产品演示 Conduct product demonstrations
+
+ C ： 举行迭代回顾 Hold iteration retrospectives
+
+ D ：开展团队建设活动 Carry out team building activities
+
+|*|*|正确答案：C 
+解析：知识点出处：敏捷实践指南 页码：P50 章节：5.2.1 回顾总结会： 回顾是最重要的一个实践，原因是它能让团队学习、改进和调整其过程。 开始是敏捷教练分配任务，在回顾会后， 团队成员可以自组织来安排任务、 改进过程。
+
+
+==-=--=
+
+
+9、  一位项目团队成员在工作场所之外与项目发起人会面后,向项目经理告知该发起人的新期望。项目经理应如何解决这个问题?
+After meeting with the project sponsor outside the workplace,a project team member update the project manager on the sponsor's new expectations.How should the project manager address this?
+
+ A ：与团队和发起人一起审查沟通管理计划 Review the communications management plan with the team and the sponsor
+
+ B ：与发起人开会,以确认期望 Meet with the sponsor to confirm expectations
+
+ C ：将发起人的期望纳入项目中 Incorporate the sponsor's expectations into the project
+
+ D ：指示团队成员不要与发起人讨论该项目 Instruct the team member not to discuss the project with the sponsor
+
+|*|*|正确答案：B 
+解析：知识点出处： PMBOK 6th 页码：P524 章节：13.2 在管理相关方参与过程中，需要开展多项活动，例如： • 在适当的项目阶段引导相关方参与，以便获取、确认或维持他们对项目成功的持续承诺； • 通过谈判沟通管理相关方期望； • 处理与相关方管理有关的任何风险或潜在关注点，预测相关方可能在未来引发的问题； • 澄清和解决已识别的问题 通过谈判与沟通了解相关方的期望。
+
+
+==-=--=
+
+
+10、  项目经理为一个具有按时完成盈利项目历史记录的组织工作。然而,由于缺乏相关方的支持以及他们未能提供信息,这些项目都经历过问题。若要避免这些问题,项目经理在新项目开始时应该做些什么?
+A project manager works for an organization with a history of completing projects on time.However,these projects have all experienced issues due to alack of stakeholder's support and their failure to provide information.What should the project manager do at the beginning of a new project to avoid these issues?
+
+ A ：管理所有相关方的期望,并为每个人提供解决方案 Manage all stakeholder's expectation,and provide a solution for each one
+
+ B ：准备一份包含所有相关方的沟通管理计划 Prepare a communications management plan that includes all stakeholders
+
+ C ： 在启动阶段识别关键相关方并进行优先级排序 Identify and prioritize the key stakeholders during the initiating stage
+
+ D ：复制之前项目的工作,因为这些项目结束时都很好 Duplicate efforts from previous projects since they ended well
+
+|*|*|正确答案：C 
+解析：知识点出处： PMBOK 6th 页码：P504 章节：13 相关方的观点：项目经理和团队正确识别并合理引导所有相关方参与的能力，能决定着项目的成败 要尽早地识别关键的相关方，因此识别相关方在启动阶段开始
+
+
 
 
 
